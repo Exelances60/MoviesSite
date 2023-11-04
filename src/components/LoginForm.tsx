fc6a5da06c7827e4a2a4bcd49501ba11b48e093f
Enes Çelik <enescelik6012@gmail.com>
2023-11-04 22:58:24 +0300

    Movie sitesi login page template

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { FC } from "react";
+import { Form, Button } from "antd";
+import { inputGroup } from "@/utils/Data/InputGroup";
+
+interface InputItemProps {
+  component: React.FC<any>;
+  name: string;
+  rules: any;
+  [key: string]: any;
+}
+
+const InputItem: FC<InputItemProps> = ({
+  component: Component,
+  name,
+  rules,
+  ...rest
+}) => (
+  <Form.Item name={name} rules={rules}>
+    <Component {...rest} />
+  </Form.Item>
+);
+
+const LoginForm = () => {
+  return (
+    <Form>
+      {inputGroup.map((item, index) => (
+        <InputItem key={index} {...item} />
+      ))}
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="login-form-button w-[100%] h-12 mt-2 bg-blue-500"
+        >
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
---
+++ src/utils/Data/InputGroup.tsx
@@ -0,0 +1,22 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Input } from "antd";
+export const inputGroup = [
+  {
+    component: Input,
+    size: "large",
+    name: "username",
+    className: "w-[100%] h-12 mt-2 ",
+    placeholder: "Username",
+    rules: [{ required: true, message: "Please input your Username!" }],
+    prefix: <UserOutlined className="site-form-item-icon" />,
+  },
+  {
+    component: Input.Password,
+    size: "large",
+    name: "password",
+    className: "w-[100%] h-12 mt-2",
+    placeholder: "Password",
+    rules: [{ required: true, message: "Please input your Password!" }],
+    prefix: <UserOutlined className="site-form-item-icon" />,
+  },
+];
